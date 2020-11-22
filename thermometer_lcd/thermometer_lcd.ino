221bd74651d55009838713e4f1c1f4ac183257c5
Oliver Poulin <oliver@bebepoulin.dk>
2020-11-22 12:54:16 +0100

    Update thermometer_lcd.ino

--- thermometer_lcd/thermometer_lcd.ino
+++ thermometer_lcd/thermometer_lcd.ino
@@ -1,3 +1,5 @@
+
+// Definere pins på arduinoen som der bliver brugt
 #define RESET 13
 #define ENABLE 12
 #define D4 11
@@ -9,46 +11,55 @@
 
 #define THERMOMETER A5
 
+// Importer LCD bibliotek
 #include <LiquidCrystal.h>
 
 // Setup LCD
-const int rs = 13, en = 12, d4 = 11, d5 = 10, d6 = 9, d7 = 8;
+const int rs = RESET, en = ENABLE, d4 = D4, d5 = D5, d6 = D6, d7 = D7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+// Definere variabler som modtager spændingen, og omregner det til temperatur
 float input = 0;
 int temp = 0;
 
+// Function der køre en gang i starten af programmet
 void setup() {
-
-  // Initialize pins
+  // Initializere analog pins
   pinMode(BUZZ, OUTPUT);
   pinMode(THERMOMETER, INPUT);
 
-  // Start LCD
+  // Starter LCD'en
   lcd.begin(20, 4);
   lcd.display();
 
 }
 
+// Function der looper i gennem programmet
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Modtager spænding fra LM35 og omregner det til Celsius
   input = analogRead(THERMOMETER);
   temp = (input*5*100)/1024;
 
+  // Nulstiller LCD
   lcd.clear();
   lcd.setCursor(0,0);
+
+  // Skriver temperatur til LCD
   lcd.print(temp);
   lcd.print("C");
 
-  
 
+  // Tester om temperatur er over grænsenieveu
   if (temp > 23) {
-    for (int i = 0; i<1000; i++){
-    digitalWrite(BUZZ, HIGH);
-    //delay(10);
-    digitalWrite(BUZZ, LOW); 
-  }  
+    // Hvis temeraturen er høj nok, bipper der en buzzer 100 gange på et sekund
+    for (int i = 0; i<100; i++){
+      digitalWrite(BUZZ, HIGH);
+      delay(5);
+      digitalWrite(BUZZ, LOW); 
+      delay(5);
+    }  
   } else {
-    delay(1000);
-    }
+      // Hvis temperature ikke er høj nok venter programmet i et sekund
+      delay(1000);
+  }
 }
